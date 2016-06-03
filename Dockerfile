d266a9538073938cfa1f6f8570cdfe319c756ac6
Spry <PSPRY@BCGOV>
2016-06-03 16:27:46 -0700

    moved jetty start into shell script

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,5 @@ RUN chmod +x /usr/local/jetty/work/tran.sh
 ENV GEOSERVER_HOME /var/lib/jetty
 ENV GEOSERVER_DATA_DIR /data
 # load data_dir if it isn't already
-RUN /usr/local/jetty/work/tran.sh
 # run jetty
-ENTRYPOINT ["java","-DGEOSERVER_DATA_DIR=/data","-Djava.awt.headless=true","-DSTOP.PORT=8079","-DSTOP.KEY=geoserver","-jar","/usr/local/jetty/start.jar"]
+ENTRYPOINT ["/usr/local/jetty/work/tran.sh"]
--- work/tran.sh
+++ work/tran.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # populate GEOSERVER_DATA_DIR from SVN if it isn't there.
-cd /data
-
+cd $GEOSERVER_DATA_DIR
 if [ ! -d workspaces ]; then
 	wget -r --no-parent -nH -X $SVNIGNOREPATH --cut-dirs=6 --user $SVN_USER --password $SVN_PASSWORD $SVNPATH 
 	chown -R 999:999 /data
 fi
+# run jetty
+cd $JETTY_BASE
+java -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar $JETTY_HOMEstart.jar
