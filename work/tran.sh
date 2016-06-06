a11f898236c984d2a9eb1ab82b52b725b4f9328e
Spry <PSPRY@BCGOV>
2016-06-06 09:33:57 -0700

    updated script to check for data mount

--- work/tran.sh
+++ work/tran.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # populate GEOSERVER_DATA_DIR from SVN if it isn't there.
-cd $GEOSERVER_DATA_DIR
-if [ ! -d workspaces ]; then
-	wget -r --no-parent -nH -X $SVNIGNOREPATH --cut-dirs=6 --user $SVN_USER --password $SVN_PASSWORD $SVNPATH 
-	chown -R 999:999 /data
+if [ -d $GEOSERVER_DATA_DIR ]; then
+	cd $GEOSERVER_DATA_DIR
+	if [ ! -d workspaces ]; then
+		wget -r --no-parent -nH -X /OGS/trunk/src/geoserver_data_dirs/DEV/INT --cut-dirs=6 --user $SVN_USER --password $SVN_PASSWORD $SVNPATH 
+		chown -R 999:999 /data
+	fi
 fi
 # run jetty
 cd $JETTY_BASE
-java -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar $JETTY_HOMEstart.jar
+java -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar $JETTY_HOME/start.jar
